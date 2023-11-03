40ce52ebfecb809bf90e7ea048fca48e58afc42d
Eunbi Yoon <yinyinbei0717@gmail.com>
2023-11-03 08:48:53 -0500

    wwerwer

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -130,7 +130,7 @@ const Contact = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    emailjs.sendForm('service_tox7kqs', 'template_nv7k7mj', form.current, 'SybVGsYS52j2TfLbi')
+    emailjs.sendForm('service_udd18xp', 'template_r9bgt0j', form.current, 't6m-wQEwFCqgB7bPN')
       .then((result) => {
         setOpen(true);
         form.current.reset();
