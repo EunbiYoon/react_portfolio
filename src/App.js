157fd3edc7ee1d3f8692675a183e6b3ce9582bd8
Software Developer <yinyinbei0717@gmail.com>
2023-11-07 20:37:04 -0600

    wer

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Hero from './components/HeroSection';
 import Skills from './components/Skills';
-import Education from './components/Edutcation';
+import Education from './components/Education';
 import Experience from './components/Experience';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
